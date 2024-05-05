af8bbf7702ab3a90ab295eca6ca9fe21c869dbbe
Daniel C <63077752+dcrep@users.noreply.github.com>
2024-05-04 21:14:39 -0400

    Game Portfolio completed

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,13 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is where I write code and ponder life.  Currently I code mostly in AutoIt, D, and JavaScript and am learning Rust and Kotlin`,
+    "cs-degree": `This is my Computer Programming Degree. I put it on the wall because there was an empty space and I don't have any paintings.`,
+    "sofa-table": `Don't dawdle Alice, we're very late, indeed!`,
+    tv: `Sometimes it's worth watching tech and programming vides on <a href="https://www.youtube.com" target="_blank">YouTube</a>`,
+    bed: `You are feeling sleepy...`,
+    resume: `No resume here!  Check out my GitHub @<a href="https://github.com/dcrep/dcrep" target="_blank">DCRep</a>`,
+    projects: `This portfolio is made in Kaboom and by using the tutorial <a href="https://www.youtube.com/watch?v=wy_fSStEgMs" target="_blank">Build a Dev Portfolio as a 2D Game - JavaScript Course</a>`,
+    library: `E-books are becoming standard fare, but physical copies of tech, programming and reference books are often worth keeping!`,
+    exit: `If you want to exit this portfolio, close the tab`,
+}
